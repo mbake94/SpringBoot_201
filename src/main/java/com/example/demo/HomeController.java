5eb86cb93cddd0bfdc7c4d684b135e466509b887
mbake94 <mbake94@gmail.com>
2020-05-18 11:03:50 -0700

    finished 2.01

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/loadform")
+    public String loadFormPage(){
+        return "formtemplate";
+    }
+    @RequestMapping("/processform")
+    public String loadFormPage(@RequestParam("login") String login, Model model){
+        model.addAttribute("loginval", login);
+        return "confirm";
+    }
+}
